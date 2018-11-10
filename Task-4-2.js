d7effe0ca4c3357915584aa4c688ac8c65649be0
Max Lutsenko <lutsenko.maxim@gmail.com>
2018-11-10 13:21:30 +0200

    Tamagochi base functions

--- Task-4-2.js
+++ Task-4-2.js
@@ -1,30 +1,52 @@
-/**
- * Created by max on 09.11.18.
- */
-var makeCounter = function() {
-    var privateCounter = 0;
-    function changeBy(val) {
-        privateCounter += val;
-    }
+// Создать тамагочи, у которого должно быть минимум 6 методов, минимум 6 переменных,
+// от которых зависит его жизнь. Например: создается инстанс тамагочи и дается имя питомцу,
+// он может кушать, гулять, спать, пить, умереть или сбежать и т.д. За эти или иные действия
+// отвечают переменные самого инстанса, например health, happiness и так далее...﻿ (constructor)
+
+
+
+function Tamagochi() {
+    var energy = 10,
+        food = 10,
+        fun = 10;
+    
     return {
-        increment: function() {
-            changeBy(1);
+        walk: function () {
+            var newEnergy = energy--;
+            food--;
+            fun = 10;
+            if (newEnergy == 0) {
+                console.log("Your pet is dead");
+            }
+
+            console.log("Energy " + energy);
+            console.log("Food " + food);
+            console.log("Fun " + fun );
         },
-        decrement: function() {
-            changeBy(-1);
+        sleep: function () {
+            energy = 10;
+            food--;
+            fun--;
+            console.log("Energy " + energy);
+            console.log("Food " + food);
+            console.log("Fun " + fun );
         },
-        value: function() {
-            return privateCounter;
+        eat: function () {
+            energy--;
+            food = 10;
+            fun--;
+            console.log("Energy " + energy);
+            console.log("Food " + food);
+            console.log("Fun " + fun );
         }
-    }
+    };
+
+
 };
 
-var Counter1 = makeCounter();
-var Counter2 = makeCounter();
-alert(Counter1.value()); /* Alerts 0 */
-Counter1.increment();
-Counter1.increment();
-alert(Counter1.value()); /* Alerts 2 */
-Counter1.decrement();
-alert(Counter1.value()); /* Alerts 1 */
-alert(Counter2.value()); /* Alerts 0 */
+var tuzik = new Tamagochi();
+
+tuzik.walk();
+tuzik.sleep();
+tuzik.eat();
+tuzik.checkStatus();
